421dbe314989191d65697a743467033304aa3759
Rocco <grigoreandrey23@gmail.com>
2022-03-21 15:44:12 +0200

    testing file opening

--- src/main.c
+++ src/main.c
@@ -2,6 +2,16 @@
 
 int main()
 {
-    printf("Hello world!");
+    FILE* file;
+    file = fopen("ceva.txt", "r");
+    if(file == NULL)
+    {
+        printf("Error opening file!");
+        return 0;
+    }
+    int var;
+    fscanf(file, "%d", &var);
+    printf("%d", var);
+    fclose(file);
     return 0;
 }
