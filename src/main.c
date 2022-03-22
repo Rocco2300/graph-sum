f456e075ab391ac41613d9ac31fcf7cc6681147d
Rocco <grigoreandrey23@gmail.com>
2022-03-22 13:27:21 +0200

    added command line arguments

--- src/main.c
+++ src/main.c
@@ -1,26 +1,23 @@
 #include <stdio.h>
 
-#include "graph_utils.h"
 #include "graph_io.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-    graph* g;
-    g = input_graph("input.txt");
-    output_graph("graph.dot", g);
-    // int** mat = build_adj_mat(g);
-    // int* indegree = build_indegree(g);
-    // int* outdegree = build_outdegree(g);
-    // print_graph(g);
-    // output_adj_mat("ceva.txt", mat, 3);
-    // output_degrees("ceva.txt", indegree, 3, 'i');
-    // output_degrees("ceva.txt", outdegree, 3, 'o');
-    // int is = get_indegree_sum(g);
-    // int os = get_outdegree_sum(g);
-    // output_sums("ceva.txt", is, os);
-    // free(indegree);
-    // free(outdegree);
-    // destroy_adj_mat(mat, 3);
-    destroy_graph(g);
+    if(argc == 1)
+    {
+        printf("No input file!\n");
+        printf("Usage: \n");
+        printf("%s [options] input_file [output_file]\n", argv[0]);
+    }
+    else if(argc == 2)
+    {
+        const char* input_path = argv[1];
+
+        graph* g;
+        g = input_graph(input_path);
+        output_requirement("ouput.txt", g);
+    }
+
     return 0;
 }
