c192306e0524f5f04ec1c2efd21c02580274ec79
Rocco <grigoreandrey23@gmail.com>
2022-03-23 09:10:03 +0200

    refactor

--- src/main.c
+++ src/main.c
@@ -6,21 +6,20 @@
 
 int main(int argc, char* argv[])
 {
-    if(argc == 1)
+    if(argc != 2)
     {
         printf("No input file!\n");
         printf("Usage: %s [options] input_file [output_file]\n", argv[0]);
     }
-    else if(argc == 2)
-    {
-        const char* input_path = argv[1];
 
-        graph* g;
-        g = input_graph(input_path);
-        output_requirement("ouput.txt", g);
-        output_graph("output.dot", g);
-        print_graph(g);
-        destroy_graph(g);
-    }
+    const char* input_path = argv[1];
+
+    graph* g;
+    g = input_graph(input_path);
+    output_requirement("ouput.txt", g);
+    output_graph("output.dot", g);
+    print_graph(g);
+    destroy_graph(g);
+
     return 0;
 }
